4c8f16ded747cf4b8197970c081c35eb06aa2863
Giovanni Silva <git@gsilva.pro>
2024-05-22 21:45:06 -0300

    databind no angular

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  msg = 'Hello'
 
+  changeMsg() {
+    this.msg = "Hello World"
+  }
 }
