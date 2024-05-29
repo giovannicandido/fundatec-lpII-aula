f9f930883185bc2252cf860099979ea9b195d589
Giovanni Silva <git@gsilva.pro>
2024-05-28 22:24:59 -0300

    usando efeitos para estilos com animacoes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimations()]
 };
--- src/app/modules/aula03/pages/databind-styles-page/databind-styles-page.component.ts
+++ src/app/modules/aula03/pages/databind-styles-page/databind-styles-page.component.ts
@@ -1,10 +1,34 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-databind-styles-page',
   templateUrl: './databind-styles-page.component.html',
-  styleUrl: './databind-styles-page.component.scss'
+  styleUrl: './databind-styles-page.component.scss',
+  animations: [
+    trigger('fade', [
+      state('visible', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('visible <=> hidden', animate('1s ease-in-out')),
+    ]),
+    trigger('color', [
+      state('blue', style({ backgroundColor: "blue" })),
+      state('red', style({ backgroundColor: "red" })),
+      state('yellow', style({ backgroundColor: "yellow" })),
+      state('brown', style({ backgroundColor: "brown" })),
+      transition('blue <=> red', animate('2s ease-in-out')),
+      transition('yellow <=> brown', animate('2s ease-in-out')),
+    ]),
+  ],
 })
 export class DatabindStylesPageComponent {
+
+  color = 'red'
+  severity: "success" | "info" = 'info'
+
+  mudarCor() {
+    this.color = this.color == 'red' ? 'blue' : 'red'
+    this.severity = this.severity == 'info' ? 'success' : 'info'
+  }
 
 }
