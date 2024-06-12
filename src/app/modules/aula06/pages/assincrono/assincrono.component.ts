4c87e9664ffe992042517e7dc64476b5d29a2df5
Giovanni Silva <git@gsilva.pro>
2024-06-11 21:21:36 -0300

    assincrono 2

--- src/app/modules/aula06/pages/assincrono/assincrono.component.ts
+++ src/app/modules/aula06/pages/assincrono/assincrono.component.ts
@@ -10,6 +10,7 @@ export class AssincronoPageComponent implements OnInit {
     observable: Observable<number> = of(1)
 
     subject = new BehaviorSubject<number>(1)
+    subject2 = new BehaviorSubject<string>("")
 
     number = 1
 
@@ -17,5 +18,12 @@ export class AssincronoPageComponent implements OnInit {
 
     ngOnInit() {
         setInterval(() => this.subject.next(this.number++), 1000)
+
+        this.subject.subscribe((valor) => console.log("Valor é: " + valor))
+    }
+
+    dobrarValor() {
+        this.number = this.number * 2;
+        this.subject2.next("Clickado em dobrar")
     }
 }
